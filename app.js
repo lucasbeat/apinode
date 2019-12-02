63a1fa0aa96e598eceb318ca6991454c813396ad
lucas beat <clashbruno10@gmail.com>
2019-12-02 12:12:28 -0300

    change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    res.send("seja bem vindo ao app")
+    res.sendFile(__dirname + "/html/index.html")
 })
 app.get("/sobre", function (req, res) {
     res.send("minha pagina sobre");
@@ -12,6 +12,4 @@ app.get("/blog", function (req, res) {
 })
 
 
-app.listen(8081, () => {
-    console.log("servidor rodando");
-});
+app.listen(8081, () => console.log("servidor rodando"))
